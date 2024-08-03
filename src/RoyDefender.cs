f01d2f40c0d7984a0bc059dbee3d4b377348062a
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-08-03 21:30:24 +0200

    file-scoped namespaces fix csproj

--- src/RoyDefender.cs
+++ src/RoyDefender.cs
@@ -1,35 +1,28 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Security.Cryptography;
-using GlobalEnums;
-using HutongGames.PlayMaker.Actions;
+﻿using HutongGames.PlayMaker.Actions;
 using Modding;
-using On.UnityEngine.UI;
 using SFCore.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace RoyDefender
+namespace RoyDefender;
+
+public class RoyDefender : Mod
 {
-    public class RoyDefender : Mod
+    public override void Initialize()
     {
-        public override void Initialize()
-        {
-            Log("Initializing");
+        Log("Initializing");
 
-            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
 
-            Log("Initialized");
-        }
+        Log("Initialized");
+    }
 
-        private void SceneManagerOnActiveSceneChanged(Scene from, Scene to)
-        {
-            if (to.name != "Dream_04_White_Defender" && to.name != "GG_White_Defender") return;
-            GameObject wdGo = to.Find("White Defender");
-            PlayMakerFSM wdFsm = wdGo.LocateMyFSM("Dung Defender");
-            wdFsm.GetAction<SendEvent>("Move Choice", 0).Enabled = true;
-            wdFsm.GetAction<SetIntValue>("Air Dive?", 3).intValue = 100;
-        }
+    private void SceneManagerOnActiveSceneChanged(Scene from, Scene to)
+    {
+        if (to.name != "Dream_04_White_Defender" && to.name != "GG_White_Defender") return;
+        GameObject wdGo = to.Find("White Defender");
+        PlayMakerFSM wdFsm = wdGo.LocateMyFSM("Dung Defender");
+        wdFsm.GetAction<SendEvent>("Move Choice", 0).Enabled = true;
+        wdFsm.GetAction<SetIntValue>("Air Dive?", 3).intValue = 100;
     }
 }
